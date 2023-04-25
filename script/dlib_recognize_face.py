924b85fad2d410b3761a9bd8b73afd3a95a8dee0
Haoran <haoranguo.me@gmail.com>
2023-04-25 13:03:15 +0800

    1

--- script/dlib_recognize_face.py
+++ script/dlib_recognize_face.py
@@ -1,19 +1,16 @@
 import dlib
 import numpy as np
-import cv2
 import csv
 import os
 
 class Recognize_Face:
-    def __init__(self, DEPTH_SCALE, LANDMARKS_MODEL, ROCOGNITION_RESNET_MODEL, FACES_FEATURES_CSV_FILE, FACES_FOLDER):
+    def __init__(self, LANDMARKS_MODEL, ROCOGNITION_RESNET_MODEL, FACES_FEATURES_CSV_FILE):
         self.detector = dlib.get_frontal_face_detector()
         self.predictor = dlib.shape_predictor(LANDMARKS_MODEL)
         self.facerec = dlib.face_recognition_model_v1(ROCOGNITION_RESNET_MODEL)
         self.FACES_FEATURES_CSV_FILE = FACES_FEATURES_CSV_FILE
-        self.FACES_FOLDER = FACES_FOLDER
-        self.DEPTH_SCALE = DEPTH_SCALE
-        if not os.path.isdir(self.FACES_FOLDER):
-            os.makedirs(self.FACES_FOLDER)
+        self.DEPTH_SCALE = 0.001
+
         # 分割出csv文件的路径和文件名
         csv_path, csv_name = os.path.split(self.FACES_FEATURES_CSV_FILE)
         # 如果csv所在的目录不存在，创建目录
@@ -150,10 +147,106 @@ class Recognize_Face:
             return False
         
         return True
+
+
+    def get_euclidean_distance(self, feature_1, feature_2):
+        feature_1 = np.array(feature_1)
+        # 将feature_2转换为float64类型
+        feature_2 = np.array(feature_2, dtype=np.float64)
+        feature_sum = 0
+        for i in range(len(feature_1)):
+            feature_1[i] = float(feature_1[i])
+            feature_2[i] = float(feature_2[i])
+            feature_sq = (feature_1[i] - feature_2[i]) ** 2
+            feature_sum += feature_sq
+        dist = np.sqrt(feature_sum)
+        return dist
+
+
+    def recognize_face_outside(self, face_descriptor, DISTANCE_THRESHOLD, csv_file):
+        database = []
+        name = ""
+        dist = 0
+        with open(csv_file, "r") as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                database.append((row[0], row[1:]))
+        
+        face_feature_distance_list = []
+        for face in database:
+            dist = self.get_euclidean_distance(face_descriptor, face[1])
+            dist = round(dist, 4)
+            if dist < DISTANCE_THRESHOLD:
+                face_feature_distance_list.append((face[0], dist))
+        
+        if len(face_feature_distance_list) == 0:
+            return False, name, dist
+        else:
+            face_feature_distance_list.sort(key=lambda x:x[1])
+            name = face_feature_distance_list[0][0]
+            dist = face_feature_distance_list[0][1]
+            return True, name, dist
+        
     
+    def recognize_face(self, face_descriptor, DISTANCE_THRESHOLD):
+        self.recognize_face_outside(face_descriptor, DISTANCE_THRESHOLD, self.FACES_FEATURES_CSV_FILE)
     
+
+    def recognize_from_2_frame(self, frame_2d, frame_3d, DISTANCE_THRESHOLD):
+        IS_PEOPLE = False
+        IS_VALIDATE = False
+        IS_RECOGNIZE = False
+        
+        face = self.detector(frame_2d, 1)
+
+        if len(face) == 0:
+            return False, None, None
+        shape = self.predictor(frame_2d, face[0])
+
+        if shape.num_parts != 68:
+            return False, None, None
+        else:
+            IS_PEOPLE = True
+
+        if not self.validate_face(frame_3d, self.DEPTH_SCALE, shape):
+            return False, None, None
+        else:
+            IS_VALIDATE = True
+
+        image = frame_2d
+
+        face_descriptor = self.facerec.compute_face_descriptor(image, shape)
+        IS_RECOGNIZE, name, dist = self.recognize_face(face_descriptor, DISTANCE_THRESHOLD)
+
+        if IS_PEOPLE and IS_VALIDATE and IS_RECOGNIZE:
+            return True, name, dist
+        else:
+            return False, None, None
+        
+    def read_image(self, IMAGE_PATH):
+        # Use dlib to load the image as a numpy array
+        image = dlib.load_rgb_image(IMAGE_PATH)
+        image = np.array(image)
+        return image
+
+
+class Extract_Face_Feature:
+    def __init__(self, LANDMARKS_MODEL, ROCOGNITION_RESNET_MODEL, FACES_FOLDER, FACES_FEATURES_CSV_FILE):
+        self.FACES_FOLDER = FACES_FOLDER
+        self.FACES_FEATURES_CSV_FILE = FACES_FEATURES_CSV_FILE
+        self.detector = dlib.get_frontal_face_detector()
+        self.predictor = dlib.shape_predictor(LANDMARKS_MODEL)
+        self.facerec = dlib.face_recognition_model_v1(ROCOGNITION_RESNET_MODEL)
+        if not os.path.isdir(self.FACES_FOLDER):
+            os.makedirs(self.FACES_FOLDER)
+        csv_path, csv_name = os.path.split(self.FACES_FEATURES_CSV_FILE)
+        # 如果csv所在的目录不存在，创建目录
+        if not os.path.isdir(csv_path):
+            os.makedirs(csv_path)
+
+
     def get_128d_features_of_face(self, image_path):
-        image = cv2.imread(image_path)
+        image = dlib.load_rgb_image(image_path)
         faces = self.detector(image, 1)
 
         if len(faces) != 0:
@@ -203,81 +296,3 @@ class Recognize_Face:
 
     def update_features_csv(self):
         self.extract_features_to_csv(self.FACES_FOLDER, self.FACES_FEATURES_CSV_FILE)
-
-
-    def get_euclidean_distance(self, feature_1, feature_2):
-        feature_1 = np.array(feature_1)
-        # 将feature_2转换为float64类型
-        feature_2 = np.array(feature_2, dtype=np.float64)
-        feature_sum = 0
-        for i in range(len(feature_1)):
-            feature_1[i] = float(feature_1[i])
-            feature_2[i] = float(feature_2[i])
-            feature_sq = (feature_1[i] - feature_2[i]) ** 2
-            feature_sum += feature_sq
-        dist = np.sqrt(feature_sum)
-        return dist
-
-
-    def recognize_face_outside(self, face_descriptor, DISTANCE_THRESHOLD, csv_file):
-        database = []
-        name = ""
-        dist = 0
-        with open(csv_file, "r") as csvfile:
-            reader = csv.reader(csvfile)
-            for row in reader:
-                database.append((row[0], row[1:]))
-        
-        face_feature_distance_list = []
-        for face in database:
-            dist = self.get_euclidean_distance(face_descriptor, face[1])
-            dist = round(dist, 4)
-            if dist < DISTANCE_THRESHOLD:
-                face_feature_distance_list.append((face[0], dist))
-        
-        if len(face_feature_distance_list) == 0:
-            return False, name, dist
-        else:
-            face_feature_distance_list.sort(key=lambda x:x[1])
-            name = face_feature_distance_list[0][0]
-            dist = face_feature_distance_list[0][1]
-            return True, name, dist
-        
-    
-    def recognize_face(self, face_descriptor, DISTANCE_THRESHOLD):
-        self.recognize_face_outside(face_descriptor, DISTANCE_THRESHOLD, self.FACES_FEATURES_CSV_FILE)
-    
-
-    def recognize_from_2_frame(self, frame_2d, frame_3d, DISTANCE_THRESHOLD, is_ir):
-        IS_PEOPLE = False
-        IS_VALIDATE = False
-        IS_RECOGNIZE = False
-        
-        face = self.detector(frame_2d, 1)
-
-        if len(face) == 0:
-            return False, None, None
-        shape = self.predictor(frame_2d, face[0])
-
-        if shape.num_parts != 68:
-            return False, None, None
-        else:
-            IS_PEOPLE = True
-
-        if not self.validate_face(frame_3d, self.DEPTH_SCALE, shape):
-            return False, None, None
-        else:
-            IS_VALIDATE = True
-
-        if is_ir:
-            image = cv2.cvtColor(frame_2d, cv2.COLOR_GRAY2BGR)
-        else:
-            image = frame_2d
-
-        face_descriptor = self.facerec.compute_face_descriptor(image, shape)
-        IS_RECOGNIZE, name, dist = self.recognize_face(face_descriptor, DISTANCE_THRESHOLD)
-
-        if IS_PEOPLE and IS_VALIDATE and IS_RECOGNIZE:
-            return True, name, dist
-        else:
-            return False, None, None
--- script/grpcServer.py
+++ script/grpcServer.py
@@ -3,6 +3,13 @@ import realGuard_pb2, realGuard_pb2_grpc
 from concurrent import futures
 import logging
 import os,time
+import numpy as np
+import dlib_recognize_face
+
+predictor_path = './model/shape_predictor_68_face_landmarks.dat'
+face_rec_model_path = './model/dlib_face_recognition_resnet_model_v1.dat'
+faces_folder = './face'
+FACES_FEATURES_CSV_FILE = './data/face_features.csv'
 
 class auth(realGuard_pb2_grpc.authServicer):
     def __init__(self) -> None:
@@ -18,19 +25,23 @@ class auth(realGuard_pb2_grpc.authServicer):
         depthData = request.depth_data
 
         #auth logic
+        image_2d = DRecFace.read_image("./pic/irImg.jpg")
+        depth = np.array(depthData)
+        is_recognized, name, dist = DRecFace.recognize_from_2_frame(image_2d, depth, 0.35)
 
         #fill in this result list
-        return realGuard_pb2.auth_result(status = 400, result = 0.89, name = "Huandong", id = "23456", instruction = 0)
+        return realGuard_pb2.auth_result(status = 400, result = dist, name = name, id = "23456", instruction = 0)
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
     realGuard_pb2_grpc.add_authServicer_to_server(auth(), server)
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port('[::]:5051')
     server.start()
     server.wait_for_termination()
 
 
 if __name__ == '__main__':
+    DRecFace = dlib_recognize_face.Recognize_Face(predictor_path, face_rec_model_path, FACES_FEATURES_CSV_FILE)
     logging.basicConfig()
     serve()
